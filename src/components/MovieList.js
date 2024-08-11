b8195a8ef8e4d9340e75337ead296e82f8d0ad28
SandhyaAvula363 <avulavenkatasandhya@gmail.com>
2024-08-11 11:54:35 +0530

    hide scroll bar

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,11 +2,11 @@ import React from 'react'
 import MovieCard from './MovieCard'
 
 const MovieList = ({title, movies}) => {
-    
+  
   return (
     <div className='px-6'>
         <h1 className='text-lg md:text-3xl py-2 text-white'>{title}</h1>
-        <div className='flex overflow-x-scroll'>
+        <div className='flex overflow-x-scroll no-scrollbar'>
             <div className='flex'>
               {movies?.map(movie => <MovieCard key={movie.id} posterPath={movie?.poster_path}/>)}
 
